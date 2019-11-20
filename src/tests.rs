2888d2ca4bf72e6c984d322861ebea5d633c6c61
jlinoff <joseph.linoff@esentire.com>
2019-11-20 08:00:19 -0800

    added another unit test

--- src/tests.rs
+++ src/tests.rs
@@ -36,4 +36,39 @@ mod tests {
 ");
         assert_eq!(output, good);
     }
+
+    #[test]
+    fn test_format_json_indent_2() {
+        // Verify that format works for a simple case
+        // with a non-default indent (2).
+        let input = String::from("{ \"key\": \"value\", \"list1\": [1, 2, 3, 4], \"list2\": [\"a\", \"b\", \"c\", \"d\"] }");
+        let opts = Opts {
+            program: String::from("my/tester"),
+            program_base: String::from("tester"),
+            depth: 32,
+            indent: 2,
+            input: None,
+            output: None,
+            verbose: 0,
+        };
+        let output = format_json(&opts, &input);
+        let good = String::from("\
+{
+  \"key\": \"value\",
+  \"list1\": [
+    1,
+    2,
+    3,
+    4
+  ],
+  \"list2\": [
+    \"a\",
+    \"b\",
+    \"c\",
+    \"d\"
+  ]
+}
+");
+        assert_eq!(output, good);
+    }
 }
